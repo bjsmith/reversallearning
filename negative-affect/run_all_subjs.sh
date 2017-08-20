1c9e705ccc02a1177af3286363d66cc7776e39d9
Ben Smith <benjsmith@gmail.com>
2017-08-20 11:48:52 -0400

    more bash

--- negative-affect/run_all_subjs.sh
+++ negative-affect/run_all_subjs.sh
@@ -1,8 +1,10 @@
 for sid in {100..400}; 
 do 
-	echo $sid;
+	#echo $sid;
 	for rid in {1..2}; 
 	do 
-		echo $rid;
+		#echo $rid;
+		echo "./run_sub.sh $sid $rid"
+		#echo "./run_sub.sh $sid $rid" | qsub
 	done
 done
