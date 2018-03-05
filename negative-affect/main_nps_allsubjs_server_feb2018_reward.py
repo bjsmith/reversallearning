2cee5bd1dd9aa2659bb2bb71dec008bdba27245a
Ben Smith <benjsmith@gmail.com>
2018-03-05 14:24:47 -0800

    a single slash

--- negative-affect/main_nps_allsubjs_server_feb2018_reward.py
+++ negative-affect/main_nps_allsubjs_server_feb2018_reward.py
@@ -2,7 +2,7 @@ from pain_regression_allsubjs import *
 rlPain=RLPain()
 
 
-rlPain.fMRI_dir='/expdata/bensmith/joint-modeling/data/msm/behavioral-analysis/reversallearning/preprocessed_fMRI_symlinks'
+rlPain.fMRI_dir='/expdata/bensmith/joint-modeling/data/msm/behavioral-analysis/reversallearning/preprocessed_fMRI_symlinks/'
 rlPain.onset_dir='/expdata/bensmith/joint-modeling/data/msm/behavioral-analysis/reversallearning/runfiles'
 #rlPain.decoder_file='/Users/benjaminsmith/GDrive/joint-modeling/reversal-learning/behavioral-analysis/data/pain_decoder.pkl'
 rlPain.nps_map_filepath = '/expdata/bensmith/joint-modeling/code/wagertools/NPS_share/weights_NSF_grouppred_cvpcr.img'
