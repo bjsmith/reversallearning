83aa86b987a3042e6ca3e3d717a0cbb797043ae3
Ben Smith <benjsmith@gmail.com>
2018-07-20 12:49:16 -0700

    dmn masks

--- negative-affect/create_subject_dmn_masks_with_standard_masks.sh
+++ negative-affect/create_subject_dmn_masks_with_standard_masks.sh
@@ -24,16 +24,15 @@ do
 			SUBJECT_INPUT="/expdata/xfgavin/MSM/sub$s/analysis/${RUN_DIR}"
 			if [ -e "$SUBJECT_INPUT" ];
 			then
+			    echo "$SUBJECT_INPUT exists; creating masks for that subject..."
 			    for mask_path in $STANDARD_MASK_DIR/*.nii.gz
 			    do
-                    echo "$SUBJECT_INPUT exists; creating mask for $mask_path."
-
                     #create link
                     #ln -s $SUBJECT_INPUT /expdata/bensmith/joint-modeling/data/msm/behavioral-analysis/reversallearning/preprocessed_fMRI_symlinks/sub${s}ReversalLearningPunishrun${r}.nii.gz
                     #ReversalLearning_Punish_run1_slicetiming_nosmooth_pre.feat
                     #flirt -applyxfm -init /expdata/xfgavin/MSM/sub106/analysis/ReversalLearning_Punish_run1_pre.feat/reg/standard2example_func.mat
                     # -in /opt/fmritools/fsl/data/standard/MNI152_T1_2mm_brain_mask.nii.gz -ref /expdata/xfgavin/MSM/sub106/analysis/ReversalLearning_Punish_run1_pre.feat/filtered_func_data.nii.gz -out /expdata/bensmith/joint-modeling/data/msm/reversallearning/masks/ReversalLearning_Punish_run1_pre_right_wholebrain_testmask.nii.gz
-                    #flirt -applyxfm -init ${SUBJECT_INPUT}/reg/standard2example_func.mat -in /opt/fmritools/fsl/data/standard/MNI152_T1_2mm_brain_mask.nii.gz -ref ${SUBJECT_INPUT}/filtered_func_data.nii.gz -out /expdata/bensmith/joint-modeling/data/msm/reversallearning/masks/wholebrain/sub${s}_${RUN_DIR}
+                    flirt -applyxfm -init ${SUBJECT_INPUT}/reg/standard2example_func.mat -in $mask_path -ref ${SUBJECT_INPUT}/filtered_func_data.nii.gz -out /expdata/bensmith/joint-modeling/data/msm/reversallearning/masks/wholebrain/sub${s}_${RUN_DIR}
                 done
 			fi
 		done
